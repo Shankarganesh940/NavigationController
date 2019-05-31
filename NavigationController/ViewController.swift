7a9ea2ea63fb4d7a72670eb59e9a17023593a050
Shankar Ganesh <shankarganesh940@gmail.com>
2019-05-31 12:45:19 +0530

    Initial Commit

---
+++ NavigationController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NavigationController
+//
+//  Created by Greeens5 on 31/05/19.
+//  Copyright © 2019 Book. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
